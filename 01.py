7db9363a04561b249390958080fb20af78d5618b
Ibrahim M <aljayabi66849@masseyhigh.school.nz>
2020-07-22 01:15:49 +0000

    I added round input

--- 01.py
+++ 01.py
@@ -1,1 +1,6 @@
-print("hello world")
+rounds = int(input("How many rounds? "))    # check with integer checker in due course
+rounds_played = 0
+
+while rounds_played < rounds:
+    print("Round {}".format(rounds_played+1))
+    rounds_played += 1

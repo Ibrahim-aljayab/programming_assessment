9848f3eec72324dfdf6ab4909df1f2d05e772b58
Ibrahim M <aljayabi66849@masseyhigh.school.nz>
2020-07-28 04:27:57 +0000

    added the whole thing

---
+++ 02_generate_numbers_between_1_and_50.py
@@ -0,0 +1,4 @@
+import random
+#Generate 2 random numbers between 10 and 30
+randomlist = random.sample(range(1, 50), 2)
+print(randomlist)
---
+++ 03_ask_user_question.py
@@ -0,0 +1,15 @@
+# asks question and compares with answer
+num_1 = 4
+num_2 = 7
+
+question = input("fill in the missing sign {} _ {}: ".format(num_1, num_2))
+if num_1 > num_2 and question == '>':
+  print ("correct")
+elif num_2 > num_1 and question == '<':
+  print("correct")
+elif num_1 == num_2 and question == "=":
+  print("Correct")
+
+else:
+  print ("wrong")
+

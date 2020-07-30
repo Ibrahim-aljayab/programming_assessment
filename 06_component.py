c86a9d7cc06796582c69c86a6b8e131497807c1f
Ibrahim M <aljayabi66849@masseyhigh.school.nz>
2020-07-30 22:15:13 +0000

    final component 6

---
+++ 06_component.py
@@ -0,0 +1,14 @@
+# HL component 6 - Statement Generator
+def hl_statement(statement, char):
+    print()
+    print(char*len(statement))
+    print(statement)
+    print(char*len(statement))
+    print
+
+# Main routine
+print()
+well_done = hl_statement("*** correct ***", "*")
+
+print()
+start_round = hl_statement("### Round 1 of 3 ###", "#")
